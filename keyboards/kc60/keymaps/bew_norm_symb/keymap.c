2eb1b796a5cd71356d4eab773ee818288d6b4c69
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2018-10-12 00:58:49 +0200

    Add normal escape key as ESC

--- keyboards/kc60/keymaps/bew_norm_symb/keymap.c
+++ keyboards/kc60/keymaps/bew_norm_symb/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [LAY_NORMAL] =
     LAYOUT_60_ansi(
-      XXXXXXX,  FR_1,     FR_2,     FR_3,     FR_4,     FR_5,     XXXXXXX,  FR_6,     FR_7,     FR_8,     FR_9,     FR_0,     KC_PAUSE, KC_BSPC, \
+      KC_ESC,   FR_1,     FR_2,     FR_3,     FR_4,     FR_5,     FR_6,     FR_7,     FR_8,     FR_9,     FR_0,     XXXXXXX,  KC_PAUSE, KC_BSPC, \
       KC_TAB,   FR_A,     FR_Z,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     FR_CIRC,  FR_EQL,   KC_DEL,  \
       KC_ESC,   FR_Q,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     FR_M,     FR_DLR,      KC_ENT,  \
            KC_LSFT,       FR_W,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     FR_COMM,  FR_SCLN,  FR_COLN,  FR_EXLM,     KC_RSFT, \
